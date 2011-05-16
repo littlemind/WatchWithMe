cf60ffa4b7c6bd9a16567e992bb2ac6e3c0a4c8f
Christoph Hoerig <christoph.hoerig@googlemail.com>
2011-05-16 19:56:01 +0200

    iniit commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WatchWithYou::Application.config.secret_token = 'd968bb619600de4967b6da3107d5ef79fbd10007617ca513fe43896eaa8b90d5b2dea195b559441e47aa290b7faebd202d06fa080716c3a3e9dde0fba5de2b47'
