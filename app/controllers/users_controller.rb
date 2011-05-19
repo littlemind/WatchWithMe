cebcce9c4f5eb33147fd4847b5f5568aa58afa2b
Christoph Hoerig <christoph.hoerig@googlemail.com>
2011-05-19 19:01:46 +0200

    identification with name in routes works

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find_by_name(params[:id])
+  end
+    
+end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-WatchWithYou::Application.config.session_store :cookie_store, :key => '_WatchWithYou_session'
+WatchWithMe::Application.config.session_store :cookie_store, :key => '_WatchWithMe_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# WatchWithYou::Application.config.session_store :active_record_store
+# WatchWithMe::Application.config.session_store :active_record_store
