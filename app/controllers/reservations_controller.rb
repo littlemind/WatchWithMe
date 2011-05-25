e467f85a8c9b37918747bb148eb79e1a53edf45e
Christoph Hoerig <christoph.hoerig@googlemail.com>
2011-05-26 00:20:33 +0200

    working basics

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,5 +1,6 @@
 class ReservationsController < ApplicationController
-  before_filter :find_user
+  before_filter :find_user, :only => [:show]
+  before_filter :authenticate_user!
   
   def show
     @reservation = Reservation.find(params[:id])
@@ -9,33 +10,38 @@ class ReservationsController < ApplicationController
   end
 
   def index
-    @reservations = Reservation.all
+    if user_signed_in?
+      @organized_reservations = current_user.organized_reservations
+      @participated_reservations = current_user.organized_reservations
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def new
-    @reservation = @user.organized_reservations.new
+    @reservation = current_user.organized_reservations.new
   end
   
   def create
     ticket_count = params[:ticket_count].to_i
-    @reservation = @user.organized_reservations.new(params[:reservation])
+    @reservation = current_user.organized_reservations.new(params[:reservation])
     if @reservation.save  
       ticket_count.times { @reservation.tickets.create }
-      redirect_to([@user,@reservation], :notice => "Reservations successfully created.")
+      redirect_to([current_user,@reservation], :notice => "Reservations successfully created.")
     else
       render :action => :new
     end
   end
 
   def edit
-    @reservation = Reservation.find(params[:id])
+    @reservation = current_user.organized_reservations.find(params[:id])
   end
   
   def update
-    @reservation = Reservation.find(params[:id])
+    @reservation = current_user.organized_reservations.find(params[:id])
     if @reservation.update_attributes(params[:reservation])
       flash[:notice] = "Successfully updated exercise."
-      redirect_to @exercise
+       redirect_to([current_user,@reservation], :notice => "Reservations successfully changed.")
     else
       render :action => 'edit'
     end
@@ -44,12 +50,13 @@ class ReservationsController < ApplicationController
   def destroy
   end
   
+  private
+  
   def find_user
     if params[:user_id]
       @user = User.find_by_username(params[:user_id])
-    else
-      @user = current_user
     end
   end
+  
 
 end
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,16 +1,17 @@
 class TicketsController < ApplicationController
-  def update
-    @ticket = Ticket.find(params[:ticket_id])
-    @ticket.update_attribute(:participant_id, current_user.id)
-    redirect_to :root
-  end
+  before_filter :authenticate_user!
   
   def create
-    
+    @ticket = Ticket.find(params[:ticket_id])
+    @ticket.update_attribute(:participant_id, current_user.id)
+    redirect_to [@ticket.reservation.organizer,@ticket.reservation]
   end
 
   def destroy
+    @ticket = current_user.tickets.find(params[:id])
+    @ticket.update_attribute(:participant_id, nil)
     
+    redirect_to([@ticket.reservation.organizer,@ticket.reservation])
   end
   
 end
