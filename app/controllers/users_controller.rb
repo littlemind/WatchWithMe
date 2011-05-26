ca25e6239cd121055a21777b6253380904051399
Christoph Hoerig <christoph.hoerig@googlemail.com>
2011-05-26 23:58:03 +0200

    uRl fix, count fix, query performance

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by_username(params[:id])
+    @user = User.find_user_by_url(params[:id])
+#    @reservations = Reservation.all(:joins => {:participants => :tickets}, :conditions => {:tickets => {:participant_id => @user.id}},:conditions => {:organizer_id => @user.id})
     @reservations = @user.participated_reservations
     @upcoming_reservations = @reservations.where("screening > ?", Time.now)
     @recent_reservations = @reservations.where("screening < ?", Time.now)
