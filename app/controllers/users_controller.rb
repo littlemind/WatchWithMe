c2a0a7ffcadbd280e56738bb44cd00e1d52f2d79
Christoph Hoerig <christoph.hoerig@googlemail.com>
2011-06-02 20:31:24 +0200

    basis private feature for reservations included

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find_user_by_url(params[:id])
 #    @reservations = Reservation.all(:joins => {:participants => :tickets}, :conditions => {:tickets => {:participant_id => @user.id}},:conditions => {:organizer_id => @user.id})
-    @reservations = @user.reservations
+    @reservations = @user.reservations.visible
     @upcoming_reservations = @reservations.upcoming
     @recent_reservations = @reservations.recent(2.week.ago).order("reserved_at DESC")
   end
